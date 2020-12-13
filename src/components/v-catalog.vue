eecc9d01bac3cb47ac04d8ec68f00a98b0131285
Yaroslav <dombrovskiy91@gmail.com>
2020-12-13 22:48:05 +0200

    add data on virtual server + vuex(actions,getters) connect in prj

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
     <h1>Catalog</h1>
     <div class="v-catalog__list">
       <v-catalog-item
-        v-for="product in products"
+        v-for="product in PRODUCTS"
         :key="product.article"
         :product_data="product"
         @sendArticle="swohArticleFromChildInConsole"
@@ -14,6 +14,7 @@
 
 <script>
 import vCatalogItem from "./v-catalog-item";
+import {mapActions, mapGetters} from 'vuex';
 
 export default {
   name: "v-catalog",
@@ -23,63 +24,24 @@ export default {
   props: {},
   data() {
     return {
-      products: [
-        {
-          image: "1.jpg",
-          name: "T-shirt 1",
-          price: 2100.234234234,
-          article: "T1",
-          available: true,
-          category: "Мужские",
-        },
-        {
-          image: "2.jpg",
-          name: "T-shirt 2",
-          price: 3150.12312412,
-          article: "T2",
-          available: true,
-          category: "Женские",
-        },
-        {
-          image: "3.jpg",
-          name: "T-shirt 3",
-          price: 4200.51524,
-          article: "T3",
-          available: false,
-          category: "Женские",
-        },
-        {
-          image: "4.jpg",
-          name: "T-shirt 4",
-          price: 5300.1245512,
-          article: "T4",
-          available: true,
-          category: "Мужские",
-        },
-        {
-          image: "5.jpg",
-          name: "T-shirt 5",
-          price: 6500.3522314,
-          article: "T5",
-          available: false,
-          category: "Женские",
-        },
-        {
-          image: "6.jpeg",
-          name: "T-shirt 6",
-          price: 8700.4124123,
-          article: "T6",
-          available: true,
-          category: "Женские",
-        },
-      ],
     };
   },
+  mounted() {
+    this.GET_PRODUCTS_FROM_API();
+  },
   methods: {
+    ...mapActions([
+      'GET_PRODUCTS_FROM_API'
+    ]),
     swohArticleFromChildInConsole(data) {
       console.log(data);
     },
   },
+  computed: {
+    ...mapGetters([
+      'PRODUCTS'
+    ]),
+  }
 };
 </script>
 
