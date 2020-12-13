57b8b55683504c5cce27782acc0118a19b55f502
Yaroslav <dombrovskiy91@gmail.com>
2020-12-13 15:47:44 +0200

    add local store data

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,16 +1,23 @@
 <template>
   <div class='v-catalog-item'>
-    <img src="" alt="img">
-    <p>Item 1</p>
-    <p>Price: 100</p>
-    <button>Add to cart</button>
+    <img :src="require('../assets/img/products/'+ product_data.image)" alt="img">
+    <p class="v-catalog-item__name">{{product_data.name}}</p>
+    <p class="v-catalog-item__price">Price: {{product_data.price}}</p>
+    <button class="v-catalog-item__add-btn btn">Add to cart</button>
   </div>
 </template>
 
 <script>
   export default {
     name: "v-catalog-item",
-    props: {},
+    props: {
+      product_data: {
+        type: Object,
+        default() {
+          return {};
+        },
+      },
+    },
     data() {
       return {}
     },
@@ -24,5 +31,9 @@
     box-shadow: 0 0 8px 0 #e0e0e0;
     padding: 16px;
     margin-bottom: 16px;
+  }
+  .v-catalog-item img{
+    max-width: 150px;
+    max-height: 150px;
   }
 </style>
