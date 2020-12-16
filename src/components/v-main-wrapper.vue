dae383440a6a7d1fd06fcd3e019e2efdfc915cab
Yaroslav <dombrovskiy91@gmail.com>
2020-12-16 15:37:29 +0200

    add Router + work Router with data + props

--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="v-main-wrapper">
-    <v-catalog/>
-    <v-cart
-        v-if="CART.length"
-        :cart_data="CART"
-    />
+  <router-view></router-view>
+    <!--    <v-catalog/>-->
+<!--    <v-cart-->
+<!--        v-if="CART.length"-->
+<!--        :cart_data="CART"-->
+<!--    />-->
   </div>
 </template>
 
 <script>
-import vCatalog from './v-catalog';
-import vCart from './v-cart';
+// import vCatalog from './v-catalog';
+// import vCart from './v-cart';
+// import router from '../router/router';
+
 import {mapGetters} from 'vuex';
 
 export default {
   name: 'v-main-wrapper',
-  components: {
-    vCatalog,
-    vCart
-  },
+  // components: {
+    // vCatalog,
+    // vCart
+  // },
   props: {},
   data() {
     return {
