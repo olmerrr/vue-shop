8ecae1ca9bca2fd9774255a52e57e77363bfc5dc
Yaroslav <dombrovskiy91@gmail.com>
2020-12-18 18:15:15 +0200

    add input range

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -6,6 +6,32 @@
         Add to Cart: {{ CART.length }}
       </div>
     </router-link>
+    <div class="filters">
+      <div class="range-slider">
+        <input
+            type="range"
+            min="0"
+            max="1000" step="10"
+            v-model.number="minPrice"
+            @change="setChangeSlider"
+        />
+        <input
+            type="range"
+            min="0"
+            max="1000"
+            step="10"
+            v-model.number="maxPrice"
+            @change="setChangeSlider"
+
+        />
+
+      </div>
+      <div class="range-values">
+        <p>Min: {{minPrice}}</p>
+        <p>Max: {{maxPrice}}</p>
+      </div>
+    </div>
+
     <div class="v-catalog__list">
       <v-catalog-item
           v-for="product in PRODUCTS"
@@ -26,10 +52,13 @@ export default {
   components: {
     vCatalogItem,
   },
-  props: {},
   data() {
-    return {};
+    return {
+      minPrice: 0,
+      maxPrice: 1000,
+    };
   },
+  props: {},
   mounted() {
     this.GET_PRODUCTS_FROM_API();
   },
@@ -41,6 +70,14 @@ export default {
     addToCart(data) {
       this.ADD_TO_CART(data)
     },
+    setChangeSlider() {
+      if (this.minPrice > this.maxPrice) {
+        let temp = this.maxPrice;
+        this.maxPrice = this.minPrice;
+        this.minPrice = temp;
+      }
+    },
+
   },
   computed: {
     ...mapGetters([
@@ -51,7 +88,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .v-catalog__list {
   display: flex;
   flex-wrap: wrap;
@@ -66,4 +103,99 @@ export default {
   padding: 16px;
   border: solid 1px #ccc;
 }
+.filters {
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+}
+input[type=range] {
+  display: inline;
+  height: 25px;
+  -webkit-appearance: none;
+  margin: 10px 0;
+}
+input[type=range]:focus {
+  outline: none;
+}
+input[type=range]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 5px;
+  cursor: pointer;
+  animate: 0.2s;
+  box-shadow: 0px 0px 0px #000000;
+  background: #2497E3;
+  border-radius: 1px;
+  border: 0px solid #000000;
+}
+input[type=range]::-webkit-slider-thumb {
+  box-shadow: 0px 0px 0px #000000;
+  border: 1px solid #2497E3;
+  height: 18px;
+  width: 18px;
+  border-radius: 25px;
+  background: #A1D0FF;
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -7px;
+}
+input[type=range]:focus::-webkit-slider-runnable-track {
+  background: #2497E3;
+}
+input[type=range]::-moz-range-track {
+  width: 100%;
+  height: 5px;
+  cursor: pointer;
+  animate: 0.2s;
+  box-shadow: 0px 0px 0px #000000;
+  background: #2497E3;
+  border-radius: 1px;
+  border: 0px solid #000000;
+}
+input[type=range]::-moz-range-thumb {
+  box-shadow: 0px 0px 0px #000000;
+  border: 1px solid #2497E3;
+  height: 18px;
+  width: 18px;
+  border-radius: 25px;
+  background: #A1D0FF;
+  cursor: pointer;
+}
+input[type=range]::-ms-track {
+  width: 100%;
+  height: 5px;
+  cursor: pointer;
+  animate: 0.2s;
+  background: transparent;
+  border-color: transparent;
+  color: transparent;
+}
+input[type=range]::-ms-fill-lower {
+  background: #2497E3;
+  border: 0px solid #000000;
+  border-radius: 2px;
+  box-shadow: 0px 0px 0px #000000;
+}
+input[type=range]::-ms-fill-upper {
+  background: #2497E3;
+  border: 0px solid #000000;
+  border-radius: 2px;
+  box-shadow: 0px 0px 0px #000000;
+}
+input[type=range]::-ms-thumb {
+  margin-top: 1px;
+  box-shadow: 0px 0px 0px #000000;
+  border: 1px solid #2497E3;
+  height: 18px;
+  width: 18px;
+  border-radius: 25px;
+  background: #A1D0FF;
+  cursor: pointer;
+}
+input[type=range]:focus::-ms-fill-lower {
+  background: #2497E3;
+}
+input[type=range]:focus::-ms-fill-upper {
+  background: #2497E3;
+}
+
 </style>
