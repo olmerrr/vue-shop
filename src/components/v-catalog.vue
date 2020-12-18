cb1c410ba23dbb99ad119193df199205a8b393ed
Yaroslav <dombrovskiy91@gmail.com>
2020-12-18 21:11:08 +0200

    add cool range slider

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -76,6 +76,8 @@ export default {
         this.maxPrice = this.minPrice;
         this.minPrice = temp;
       }
+
+
     },
 
   },
@@ -89,113 +91,62 @@ export default {
 </script>
 
 <style scoped>
-.v-catalog__list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
+  .v-catalog__list {
+     display: flex;
+     flex-wrap: wrap;
+     justify-content: space-between;
+     align-items: center;
+   }
+  .v-catalog__link-to-cart {
+     position: fixed;
+     top: 10px;
+     right: 10px;
+     padding: $padding*2;
+     border: solid 1px #aeaeae ;
+     background: #ffffff;
+   }
+  .filters {
+    padding: 20px 0;
+    display: flex;
+    justify-content: space-between;
+  }
+  .range-slider input[type=range] {
+    position: absolute;
+    left: 65%;
+    top: 180px;
+  }
+  ::-webkit-slider-thumb {
+    z-index: 2;
+    position: relative;
+    top: 2px;
+    margin-top: -7px;
+  }
+  input[type=range]{
+    -webkit-appearance: none;
+  }
+  input[type=range]::-webkit-slider-runnable-track {
+    width: 300px;
+    height: 5px;
+    background: #ddd;
+    border: none;
+    border-radius: 3px;
+  }
+  input[type=range]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    border: 1.5px solid #C1C1C1;
+    height: 16px;
+    width: 16px;
+    border-radius: 50%;
+    background: #EDEDED;
+  }
+  input[type=range]:focus {
+    outline: none;
+  }
+  input[type=range]::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 5px;
+    box-shadow: 1px 1px 1px #C6C6C6, 0px 0px 1px #787878;
+    border-radius: 2px;
+    border: 0.2px solid #787878;
 }
-
-.v-catalog__link-to-cart {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  padding: 16px;
-  border: solid 1px #ccc;
-}
-.filters {
-  padding: 20px 0;
-  display: flex;
-  justify-content: space-between;
-}
-input[type=range] {
-  display: inline;
-  height: 25px;
-  -webkit-appearance: none;
-  margin: 10px 0;
-}
-input[type=range]:focus {
-  outline: none;
-}
-input[type=range]::-webkit-slider-runnable-track {
-  width: 100%;
-  height: 5px;
-  cursor: pointer;
-  animate: 0.2s;
-  box-shadow: 0px 0px 0px #000000;
-  background: #2497E3;
-  border-radius: 1px;
-  border: 0px solid #000000;
-}
-input[type=range]::-webkit-slider-thumb {
-  box-shadow: 0px 0px 0px #000000;
-  border: 1px solid #2497E3;
-  height: 18px;
-  width: 18px;
-  border-radius: 25px;
-  background: #A1D0FF;
-  cursor: pointer;
-  -webkit-appearance: none;
-  margin-top: -7px;
-}
-input[type=range]:focus::-webkit-slider-runnable-track {
-  background: #2497E3;
-}
-input[type=range]::-moz-range-track {
-  width: 100%;
-  height: 5px;
-  cursor: pointer;
-  animate: 0.2s;
-  box-shadow: 0px 0px 0px #000000;
-  background: #2497E3;
-  border-radius: 1px;
-  border: 0px solid #000000;
-}
-input[type=range]::-moz-range-thumb {
-  box-shadow: 0px 0px 0px #000000;
-  border: 1px solid #2497E3;
-  height: 18px;
-  width: 18px;
-  border-radius: 25px;
-  background: #A1D0FF;
-  cursor: pointer;
-}
-input[type=range]::-ms-track {
-  width: 100%;
-  height: 5px;
-  cursor: pointer;
-  animate: 0.2s;
-  background: transparent;
-  border-color: transparent;
-  color: transparent;
-}
-input[type=range]::-ms-fill-lower {
-  background: #2497E3;
-  border: 0px solid #000000;
-  border-radius: 2px;
-  box-shadow: 0px 0px 0px #000000;
-}
-input[type=range]::-ms-fill-upper {
-  background: #2497E3;
-  border: 0px solid #000000;
-  border-radius: 2px;
-  box-shadow: 0px 0px 0px #000000;
-}
-input[type=range]::-ms-thumb {
-  margin-top: 1px;
-  box-shadow: 0px 0px 0px #000000;
-  border: 1px solid #2497E3;
-  height: 18px;
-  width: 18px;
-  border-radius: 25px;
-  background: #A1D0FF;
-  cursor: pointer;
-}
-input[type=range]:focus::-ms-fill-lower {
-  background: #2497E3;
-}
-input[type=range]:focus::-ms-fill-upper {
-  background: #2497E3;
-}
-
 </style>
